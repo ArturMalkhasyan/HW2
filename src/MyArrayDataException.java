b0cb923e73c1e9c93dad79a51f3c0cf9a6cc2ad4
Artur Malhayan <reaze@mail.ru>
2021-02-04 22:05:47 +0300

    Initial commit

---
+++ src/MyArrayDataException.java
@@ -0,0 +1,9 @@
+public class MyArrayDataException extends RuntimeException {
+    int i, j;
+
+    public MyArrayDataException(int i, int j) {
+        super("Неправильные данные. У тебя есть: row " + i + ", column " + j);
+        this.i = i;
+        this.j = j;
+    }
+}
